fa42c22fad774123cc54a4075a257e11788be797
a9it.Nirav <a9it.nirav@gmail.com>
2024-08-17 19:21:54 +0530

    service page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MyNav from './compoenets/MyNav'
-import  Carousel  from './compoenets/Carousel';
+import Carousel from './compoenets/Carousel';
 import Home1 from './compoenets/Home1';
 import Footer from './compoenets/Footer';
 import Services from './compoenets/Services/Services';
@@ -17,30 +17,30 @@ function App() {
 
   return (
     <>
-    {/* <MyNav/>
+      {/* <MyNav/>
     <Carousel/>
     <Home1/>
     <Footer/>
     <Services/> */}
 
 
-<BrowserRouter> 
-<Routes>
-  <Route path='/' element={<Layout/>}>
-  <Route path='/' element={<Home/>}/>
-  <Route path='/ServiceListing' element={<ServiceListing/>}/>
- 
-  </Route>
-
-</Routes>
-</BrowserRouter>
-
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="ServiceListing" element={<ServiceListing />}>
+              <Route path=":id" element={<Services />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
 
-  
-  
-  
+
+
+
+
+
 
 
     </>
--- Frontend/src/compoenets/Services/Services.jsx
+++ Frontend/src/compoenets/Services/Services.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function Services() {
+
+  const {id} = useParams();
+
+  const Services = [
+    {
+        id: 1,
+        name: "Skin Care Centres",
+        img: 'Skincare-Centres1.jpg'
+    },
+    {
+        id: 2,
+        name: "Test Tube Baby Centres",
+        img: 'Test-Tube-Baby-Centres1.jpg'
+    },
+    {
+        id: 3,
+        name: "Kidney Care Centres",
+        img: 'Kidney-care-Centres.png'
+    },
+    {
+        id: 4,
+        name: "Cancer Care Centres",
+        img: 'Cancer-Centres.jpg'
+    },
+    {
+        id: 5,
+        name: "Plastic Surgery",
+        img: 'specialities/Plastic-Surgery.jpg'
+    },
+    {
+        id: 6,
+        name: "Pulmonology",
+        img: 'specialities/Pulmonology.jpg'
+    }
+
+]
+
   return (
     <div>
-        i am services
+      {
+        Services.map(e=>(
+          <div className="">{id} - {e.name}</div>
+        ))
+      }
       
     </div>
   )
