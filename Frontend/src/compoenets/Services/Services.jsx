dcff4c38944a4cee66e06be1909113b70c544e0c
a9it.Nirav <a9it.nirav@gmail.com>
2024-08-21 14:20:32 +0530

    create listing page

--- Frontend/src/compoenets/Services/Services.jsx
+++ Frontend/src/compoenets/Services/Services.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Services() {
   const path = import.meta.env.VITE_APP_IMG_URL;
-  const [servicesList,setServicesList] = useState([
+  const [servicesList, setServicesList] = useState([
     { id: 1, name: "Skin Care Centres", img: 'Skincare-Centres1.jpg' },
     { id: 2, name: "Test Tube Baby Centres", img: 'Test-Tube-Baby-Centres1.jpg' },
     { id: 3, name: "Kidney Care Centres", img: 'Kidney-care-Centres.png' },
@@ -28,19 +28,74 @@ function Services() {
 
   ]);
 
+  // Function to sort services Z to A
+  const sortZToA = () => {
+    const sortedList = [...servicesList].sort((a, b) => b.name.localeCompare(a.name));
+    setServicesList(sortedList);
+  };
+
+
+  // Function to sort services A to Z
+  const sortAToZ = () => {
+    const sortedList = [...servicesList].sort((a, b) => a.name.localeCompare(b.name));
+    setServicesList(sortedList);
+  };
+
+
+
+
   return (
-    <div className="row">
-      {servicesList.map(service => (
-        <div className="col-md-3 col-6" key={service.id}>
-          <Link to={`/ServiceListing/service/${service.id}`} className="a-links">
-            <div>
-              <img src={`${path}${service.img}`} alt={service.name} className="img-fluid services-img" />
+    <>
+
+
+      <div>
+        <img src={path + 'banner-new1.jpg'} alt="" className='img-fluid' />
+      </div>
+      <div className="container margin-top3">
+        <div className="row d-flex justify-content-between">
+          <div className="col-md-6 col-8">
+            <h2 className='homepage-section-heading'>Healthcare Services</h2>
+            {/* <div className="home-page-semi-head">Private online consultations with verified doctors in all specialists</div> */}
+
+
+          </div>
+
+
+          <div className="col-md-3 col-4 text-end d-flex justify-content-end align-items-center">
+
+            <div class="dropdown ">
+              <a class=" btn-small dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                Short By
+              </a>
+
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <li><a className="dropdown-item" href="#" onClick={sortAToZ}>Sort by A to Z</a></li>
+                <li><a className="dropdown-item" href="#" onClick={sortZToA}>Sort by Z to A</a></li>
+              </ul>
             </div>
-            <p className="semi-head1 mb-0">{service.name}</p>
-          </Link>
+
+          </div>
+
         </div>
-      ))}
-    </div>
+
+
+        <div className="row g-3 mt-1">
+          {servicesList.map(service => (
+            <div className="col-md-3 col-6" key={service.id}>
+              <Link to={`/ServiceListing/service/${service.id}`} className="a-links">
+                <div>
+                  <img src={`${path}${service.img}`} alt={service.name} className="img-fluid services-img" />
+                </div>
+                <p className="semi-head1 mb-0 ">{service.name}</p>
+              </Link>
+            </div>
+          ))}
+
+        </div >
+
+
+      </div>
+    </>
   );
 }
 
