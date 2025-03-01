835a3dc610cd922f5859410344f984524f649e8a
Kaushikk52 <kaushikkarnik635@gmail.com>
2025-03-01 13:38:50 +0530

    title on listing change

--- Frontend/src/components/pages/Listing/ServiceListing.jsx
+++ Frontend/src/components/pages/Listing/ServiceListing.jsx
@@ -387,7 +387,7 @@ export default function ServiceListing() {
       {/* Main Content */}
       <div className="max-w-7xl mx-auto px-4 py-6">
         <div className="flex justify-between items-center mb-6">
-              <h2 className="text-2xl font-bold capitalize">Hospitals in {location  }</h2>
+              <h2 className="text-2xl font-bold capitalize">Hospitals in {location || "Mumbai"}</h2>
               <button
                 onClick={() => setFilterOpen(true)}
                 className="md:hidden flex items-center gap-2 px-4 py-2 border rounded-lg hover:bg-gray-50"
