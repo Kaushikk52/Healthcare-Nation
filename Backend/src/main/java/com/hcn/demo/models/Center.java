4f718ce37f8367a677d934cc696cfc17f2642e11
Kaushikk52 <kaushikkarnik635@gmail.com>
2025-04-04 12:08:36 +0530

    center backend almost done

---
+++ Backend/src/main/java/com/hcn/demo/models/Center.java
@@ -0,0 +1,119 @@
+package com.hcn.demo.models;
+
+import com.hcn.demo.helper.StringListConverter;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Center {
+
+    @Id
+    @Column(name = "id", nullable = false, updatable = false, length = 36)
+    private String id;
+
+    @NotNull(message = "Hospital name cannot be null")
+    @Size(min = 3, max = 100, message = "Hospital name must be between 3 and 100 characters")
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+    private String website;
+    private String openDay;
+    private String closeDay;
+    private String hours;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Lob
+    @Column(name = "description",columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "phoneNumbers")
+    @Size(min = 1, message = "At least one phone number is required.")
+    private String[] phoneNumbers;
+
+    @Column(name = "facts", length = 5000, columnDefinition = "VARBINARY(5000)")
+    private String[] facts;
+    @Column(name = "achievements", length = 5000, columnDefinition = "VARBINARY(5000)")
+    private String[] achievements;
+
+    @Column(name = "images", length = 5000, columnDefinition = "VARBINARY(5000)")
+    private String[] images;
+    @Column(name = "videos", length = 5000, columnDefinition = "VARBINARY(5000)")
+    private String[] videos;
+
+    @Enumerated(EnumType.STRING)
+    private OwnershipType ownership;
+
+    @Enumerated(EnumType.STRING)
+    private CenterType type;
+
+    @Column(name = "brands", columnDefinition = "TEXT")
+    @Convert(converter = StringListConverter.class)
+    private List<String> brands;
+    @Column(name = "diagnostics", columnDefinition = "TEXT")
+    @Convert(converter = StringListConverter.class)
+    private List<String> diagnostics;
+    @Column(name = "specialities", columnDefinition = "TEXT")
+    @Convert(converter = StringListConverter.class)
+    private List<String> specialities;
+    @Column(name = "services", columnDefinition = "TEXT")
+    @Convert(converter = StringListConverter.class)
+    private List<String> services;
+    @Column(name = "psu", columnDefinition = "TEXT")
+    @Convert(converter = StringListConverter.class)
+    private List<String> psu;
+    @Column(name = "accreditations", columnDefinition = "TEXT")
+    @Convert(converter = StringListConverter.class)
+    private List<String> accreditations;
+    @Column(name = "concerns", columnDefinition = "TEXT")
+    @Convert(converter = StringListConverter.class)
+    private List<String> concerns;
+    @Column(name = "insurance", columnDefinition = "TEXT")
+    @Convert(converter = StringListConverter.class)
+    private List<String> insurance;
+    @Column(name = "tpa", columnDefinition = "TEXT")
+    @Convert(converter = StringListConverter.class)
+    private List<String> tpa;
+    @Column(name = "altMed", columnDefinition = "TEXT")
+    @Convert(converter = StringListConverter.class)
+    private List<String> altMed;
+
+    private Boolean isSaved;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public enum OwnershipType{
+        PRIVATE,GOVERNMENT
+    }
+
+    public enum CenterType{
+            dialysis,ivf,burns,hairTransplant,checkup,rehabilitation
+    }
+
+    @PrePersist
+    private void prePersist() {
+        if (id == null) {
+            id = UUID.randomUUID().toString();
+        }
+    }
+
+
+}
---
+++ Backend/src/main/java/com/hcn/demo/services/CenterService.java
@@ -0,0 +1,67 @@
+package com.hcn.demo.services;
+
+import com.hcn.demo.models.Center;
+import com.hcn.demo.models.User;
+import com.hcn.demo.repositories.CenterRepo;
+import com.hcn.demo.specifications.GenericSpecification;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CenterService {
+
+    @Autowired
+    private CenterRepo centerRepo;
+
+    @Autowired
+    private ImageService imageServ;
+
+
+    public Center addCenter(Center center, User principalUser){
+        center.setUser(principalUser);
+        Center savedCenter = centerRepo.save(center);
+        return savedCenter;
+    }
+
+    public List<Center> getAll(){
+        return centerRepo.findAll();
+    }
+
+    public List<Center> getAllByType(String type){
+        return centerRepo.findByType(Center.CenterType.valueOf(type));
+    }
+
+    public Center getById(String id){
+        return centerRepo.findById(id).orElseThrow(() -> new RuntimeException("Not found ..."));
+    }
+
+    public List<Center> getFilteredCenters(Map<String,Object> filters){
+        Specification<Center> spec = GenericSpecification.findByCriteria(filters);
+        List<Center> filteredCenters = centerRepo.findAll(spec);
+        return filteredCenters;
+    }
+
+    public Center updateCenter(Center center,List<String> deleteImages){
+        Center existingCenter =  centerRepo.findById(center.getId()).orElseThrow(() -> new RuntimeException("Not found ..."));
+        List<String> results = imageServ.deleteFiles(deleteImages,"Hospitals");
+        BeanUtils.copyProperties(center,existingCenter,"createdAt");
+        existingCenter.setUpdatedAt(LocalDateTime.now());
+        return centerRepo.save(existingCenter);
+    }
+
+    public void deleteCenter(String id){
+        Center existingCenter =  centerRepo.findById(id).orElseThrow(() -> new RuntimeException("Not found ..."));
+        List<String> results = imageServ.deleteFiles(List.of(existingCenter.getImages()),"Hospitals");
+        centerRepo.delete(existingCenter);
+
+    }
+
+
+
+}
